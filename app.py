c25d57863765766165cff2dcda2e80bf696e6de1
Yazeedalyan <yazeedalyan2@gmail.com>
2022-08-08 16:14:36 +0300

    s

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ config = {
   "storageBucket": "case-study-yalla-reyada.appspot.com",
   "messagingSenderId": "395694763700",
   "appId": "1:395694763700:web:5219bacf7b4b0fe5adf1e1",
-  "measurementId": "G-J6PGT8PB2D"
-  "databaseURL":"https://case-study-yalla-reyada-default-rtdb.europe-west1.firebasedatabase.app/"
-}
+  "measurementId": "G-J6PGT8PB2D",
+  "databaseURL":"https://case-study-yalla-reyada-default-rtdb.europe-west1.firebasedatabase.app"}
+
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
@@ -41,4 +41,4 @@ def for_you_trainee():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)1
+    app.run(debug=True)
