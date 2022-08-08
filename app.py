71c19d5a86402367e3d682a5471db05b64fd82ff
Noam Cohen-Ofir <109899445+noamco23-meet@users.noreply.github.com>
2022-08-08 15:25:49 +0300

    added database and firebase config

--- app.py
+++ app.py
@@ -13,77 +13,16 @@ config = {
   "messagingSenderId": "395694763700",
   "appId": "1:395694763700:web:5219bacf7b4b0fe5adf1e1",
   "measurementId": "G-J6PGT8PB2D"
-  "databaseURL":""
+  "databaseURL":"https://case-study-yalla-reyada-default-rtdb.europe-west1.firebasedatabase.app/"
 }
+
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 db = firebase.database()
-@app.route('/home')
-def home():
-    return render_template("index.html")
 
-@app.route('/', methods=['GET', 'POST'])
-def signin():
-    if request.method == 'POST':
-       email = request.form['email']
-       password = request.form['password']
-       try:
-            login_session['user'] = auth.sign_in_with_email_and_password(email, password)
-            return render_template("index.html")
-       except:
-           error = "Authentication failed"
-    return render_template("signin.html")
-
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-    error  =""
-    if request.method == 'POST':
-        email = request.form['email']
-        password = request.form['password']
-        full_name = request.form['full_name']
-        username = request.form['username']
-        class_of = request.form['class']
-
-        try:
-            login_session['user'] = auth.create_user_with_email_and_password(email, password)
-            user = {"email": email, "password": password, "full_name": full_name, "username": username, "class_of": class_of}
-            db.child("Users").child(login_session['user']['localId']).set(user)
-            return render_template("index.html")
-        except:
-           error = "Authentication failed"
-           print(error)
-    return render_template("signup.html")
+@app.route('/')
 
 
-@app.route('/add_tweet', methods=['GET', 'POST'])
-def add_tweet():
-    error  =""
-    if request.method == 'POST':
-        year = request.form['year']
-        journey = request.form['journey']
-        project = request.form['project']
-        today = datetime.now()
-        today = today.strftime("%d/%m/%Y %H:%M:%S")
-        try:
-            tweet = {"year": year, "journey": journey, 'project': project, 'time': today}
-            db.child("Tweet").push(tweet)
-            return redirect(url_for('all_tweet'))
-        except:
-           error = "Authentication failed"
-           print(error)
-    return render_template("add_tweet.html")
 
-
-@app.route('/all_tweets', methods=['GET', 'POST'])
-def all_tweet():
-    all_tweets2 = db.child("Tweet").get().val().values()
-    return render_template("all_tweets.html", all_tweets2 = all_tweets2)
-
-
-@app.route('/signout', methods=['GET', 'POST'])
-def signout():
-    login_session['user'] = None
-    auth.current_user = None
-    return render_template("signin.html")
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True)1
